3c6d646732a7fca1727dc3a3363dcbb2fdd1faa9
IdentityNulll <identitynulll@gmail.com>
2026-07-24 20:17:06 +0500

    google client

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL:  'https://til-up-production.up.railway.app/api',
+  baseURL: 'https://til-up-production.up.railway.app/api',
 });
 
 axiosClient.interceptors.request.use((config) => {
